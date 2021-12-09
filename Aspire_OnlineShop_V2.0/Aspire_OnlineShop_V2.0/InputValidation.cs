5becfeff7718af98c3164f0f17ca39eb5ffc1460
Juan Carlos Glez <54123331+JKGlez@users.noreply.github.com>
2021-12-08 18:27:33 -0600

    Add files via upload

---
+++ Aspire_OnlineShop_V2.0/Aspire_OnlineShop_V2.0/InputValidation.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aspire_OnlineShop_V2._0
+{
+    class InputValidation
+    {
+        public Product ValidatingProdcutByID(List<Product> theProducts)
+        {
+            try
+            {
+                int selectedID = int.Parse(Console.ReadLine());
+
+                IEnumerable<Product> productD = from product in theProducts
+                                                where product.Id == selectedID
+                                                select product;
+                if (productD.Count() != 0)
+                    return productD.First();
+                else
+                {
+                    Console.WriteLine("ID not found, try again:");
+                    return ValidatingProdcutByID(theProducts);
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("The ID can't have Letters (FormatException), try again:");
+                return ValidatingProdcutByID(theProducts);
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("The ID can't be so BIG (OverflowException), try again:");
+                return ValidatingProdcutByID(theProducts);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("General exception ocurred:\n"+ex.Message+"\ntry again:");
+                return ValidatingProdcutByID(theProducts);
+            }
+        }
+
+        public int ValidatingID(List<Product> theProducts)
+        {
+            try
+            {
+                int selectedID = int.Parse(Console.ReadLine());
+
+                IEnumerable<Product> productD = from product in theProducts
+                                                where product.Id == selectedID
+                                                select product;
+                if (productD.Count() != 0)
+                    return productD.First().Id;
+                else
+                {
+                    Console.WriteLine("ID not found, try again:");
+                    return ValidatingID(theProducts);
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("The ID can't have Letters (FormatException), try again:");
+                return ValidatingID(theProducts);
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("The ID can't be so BIG (OverflowException), try again:");
+                return ValidatingID(theProducts);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("General exception ocurred:\n" + ex.Message + "\ntry again:");
+                return ValidatingID(theProducts);
+            }
+        }
+
+        //CHECK VALIDATION OF DATA
+        public Employee ValidatingNewEmployee()
+        {
+            try
+            {
+                string tryEmpoyee = Console.ReadLine();
+                string[] tryEmpoyeeData = tryEmpoyee.Split(',');
+
+                string name = tryEmpoyeeData[0];
+                string email = tryEmpoyeeData[1];
+                long mobile = long.Parse(tryEmpoyeeData[2]);
+                string addres = tryEmpoyeeData[3];
+                string password = tryEmpoyeeData[4];
+                bool adminR = tryEmpoyeeData[5].ToLower().Equals("true");
+
+                Employee newEmployee= new Employee(name, email, mobile, addres, password, adminR);
+                
+                return newEmployee;
+
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return ValidatingNewEmployee();
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return ValidatingNewEmployee();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return ValidatingNewEmployee();
+            }
+        }
+        public Employee ValidatingExitedEmployee(List<Employee> theEmployees)
+        {
+            try
+            {
+                int tryE = ValidatingNumberRange(0, theEmployees.Count);
+
+                return theEmployees[tryE];
+
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return ValidatingNewEmployee();
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return ValidatingNewEmployee();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return ValidatingNewEmployee();
+            }
+        }
+
+        public bool ValidatingPassword(Employee theEmployee)
+        {
+            try
+            {
+                string inputString = (Console.ReadLine());
+
+                if (inputString.Equals(theEmployee.Password))
+                {
+                    return true;
+                }
+
+                else
+                {
+                    int count = 1;
+                    do
+                    {
+                        Console.WriteLine("Invalid Password, attempt ("+count+"/3)");
+                        inputString = (Console.ReadLine());
+
+                        if (inputString.Equals(theEmployee.Password))
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            count++;
+                        }
+                    } while (count < 3);
+                    Console.WriteLine("Access DENIED.");
+                    return false;
+                }
+
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return ValidatingPassword(theEmployee);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return ValidatingPassword(theEmployee);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return ValidatingPassword(theEmployee);
+            }
+        }
+            
+        public string ValidatingYesNo()
+        {
+            try
+            {
+                string inputString = (Console.ReadLine());
+
+                if (inputString.ToLower().Equals("yes"))
+                {
+                    return "yes";
+                }
+                else if (inputString.ToLower().Equals("no"))
+                {
+                    return "no";
+                }
+                else
+                {
+                    Console.WriteLine("Invalid answer, try again:");
+                    return ValidatingYesNo();
+                }
+                    
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return ValidatingYesNo();
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return ValidatingYesNo();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return ValidatingYesNo();
+            }
+        }
+
+        public int ValidatingNumberRange(int bN, int tN)
+        {
+            try
+            {
+                int tryInt = int.Parse(Console.ReadLine());
+
+                if (tryInt >= bN && tryInt <= tN)
+                {
+                    return tryInt;
+                }
+                else
+                {
+                    Console.WriteLine("Number out of range, try again:");
+                    return ValidatingNumberRange(bN, tN);
+                }
+
+
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return ValidatingNumberRange(bN,tN);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return ValidatingNumberRange(bN, tN);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return ValidatingNumberRange(bN, tN);
+            }
+        }
+    }
+}
+
---
+++ Aspire_OnlineShop_V2.0/Aspire_OnlineShop_V2.0/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aspire_OnlineShop_V2._0
+{
+    abstract class Person
+    {
+        //FIELDS
+        private string name;
+        private string email;
+        private long mobile;
+        private string addres;
+
+        //PROPIERTIES
+        public string Name { get => name; set => name = value; }
+        public string Email { get => email; set => email = value; }
+        public long Mobile { get => mobile; set => mobile = value; }
+        public string Addres { get => addres; set => addres = value; }
+
+        //CONSTRUCTOR
+        protected Person(string name, string email, long mobile, string addres)
+        {
+            this.name = name;
+            this.email = email;
+            this.mobile = mobile;
+            this.addres = addres;
+        }
+
+        public override string ToString()
+        {
+            return (Name + "\t" + Email + "\t" + Mobile);
+
+
+
+        }
+    }
+}
